32d24730200038a7702aba3fd1d177bc9e81e2ae
Shlomo <syivry04@gmail.com>
2022-01-03 15:51:12 +0200

    javascript

---
+++ Mixed_Messages/mixedmessages.js
@@ -0,0 +1,6 @@
+
+const magic8Ball = ['It won\'t happen', 'If that is what you want', 'I wouldn\'t ask that question', 'There\'s a 50% chance of it happening', 'There is no answer', 'It\'s very likely for it to happen', 'Forsure', 'The stars say no']
+
+    let randomNum = Math.floor(Math.random()*8)
+
+return magic8Ball[randomNum]
